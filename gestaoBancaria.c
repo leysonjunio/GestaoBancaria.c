611aa9f6d1b69ce94cad8d0962a3e5905e506a68
Leyson junio <148728893+leysonjunio@users.noreply.github.com>
2026-02-13 09:29:34 -0300

    estou estudado a ler/escreve dados em C

--- gestaoBancaria.c
+++ gestaoBancaria.c
@@ -5,6 +5,20 @@ int trasferencia(){return 0;}
 int saldoDaConta(){return 0;}
 int login(){return 0;}
 
+typedef struct{
+    int id, telefone;
+    char nome[50], email[30], cpf[12];
+}person;
+
 int main(){
+    FILE * infile;
+
+    infile = fopen("person.dat", "wb+");
+    if(infile){
+        person cliente1 = {1, 34413333, "joao vitor", "joao@email.com", "01010101"}; 
+        fwrite(&cliente1, sizeof(person), 1, infile);
+        fclose(infile);
+    }
+
     return 0;
 }
